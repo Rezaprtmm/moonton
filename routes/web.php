6caf0c929698bbc998597396083f95aa017c5b9c
Rezaprtmm <rizki.pratama@students.paramadina.ac.id>
2023-06-09 19:33:51 +0700

    feat: slicing subscription plan page

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,11 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
     route::get('/dashboard', function () {
         return Inertia::render('Prototype/Dashboard');
     })->name('dashboard');
+
+    //route payments
+    route::get('/subscriptionPlan', function () {
+        return Inertia::render('Prototype/SubscriptionPlan');
+    })->name('subscriptionPlan');
 });
 
 
