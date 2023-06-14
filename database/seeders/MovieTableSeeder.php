cf123ce7214e422f484698cc397d90d3beed35b7
Rezaprtmm <rizki.pratama@students.paramadina.ac.id>
2023-06-14 21:48:49 +0700

    feat: integration dashboard

--- database/seeders/MovieTableSeeder.php
+++ database/seeders/MovieTableSeeder.php
@@ -19,8 +19,8 @@ class MovieTableSeeder extends Seeder
                 'slug' => 'the-shawshank-redemption',
                 'category' => 'drama',
                 'video_url' => 'https://www.youtube.com/watch?v=6hB3S9bIaco',
-                'thumbnail' => 'https://i.ytimg.com/vi/6hB3S9bIaco/maxresdefault.jpg',
-                'rating' => 9.3,
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg',
+                'rating' => 4.3,
                 'is_featured' => 1,
             ],
             [
@@ -29,7 +29,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'drama',
                 'video_url' => 'https://www.youtube.com/watch?v=sY1S34973zA',
                 'thumbnail' => 'https://i.ytimg.com/vi/sY1S34973zA/maxresdefault.jpg',
-                'rating' => 9.2,
+                'rating' => 4.2,
                 'is_featured' => 0,
             ],
             [
@@ -38,7 +38,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'action',
                 'video_url' => 'https://www.youtube.com/watch?v=EXeTwQWrcwY',
                 'thumbnail' => 'https://i.ytimg.com/vi/EXeTwQWrcwY/maxresdefault.jpg',
-                'rating' => 9.0,
+                'rating' => 4.0,
                 'is_featured' => 0,
             ]
             ];
