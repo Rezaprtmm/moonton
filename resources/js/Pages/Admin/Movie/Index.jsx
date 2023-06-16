24b87cc781948f89b4f6e6b3d39d03d3bc34beb9
Rezaprtmm <rizki.pratama@students.paramadina.ac.id>
2023-06-16 21:48:11 +0700

    feat: admin update movie

--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -3,7 +3,7 @@ import PrimaryButton from "@/Components/PrimaryButton";
 import Authenticated from "@/Layouts/Auntheticated/Index";
 import { Link } from "@inertiajs/react";
 
-export default function Index({ auth, flashMessage }) {
+export default function Index({ auth, flashMessage, movies }) {
     return (
         <Authenticated user={auth.user}>
             <div className="w-40">
@@ -16,6 +16,53 @@ export default function Index({ auth, flashMessage }) {
             {flashMessage?.message && (
                 <FlashMessage message={flashMessage.message} />
             )}
+            <table className="table-fixed w-full text-center">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Rating</th>
+                        <th colSpan={2}>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => (
+                        <tr key={movie.id}>
+                            <td>
+                                <img
+                                    src={`/storage/${movie.thumbnail}`}
+                                    alt={movie.name}
+                                    className="w-23 rounded-md"
+                                />
+                            </td>
+                            <td>{movie.name}</td>
+                            <td>{movie.category}</td>
+                            <td>{movie.rating.toFixed(1)}</td>
+                            <td>
+                                <Link
+                                    href={route(
+                                        "admin.dashboard.movie.edit",
+                                        movie.id
+                                    )}
+                                >
+                                    <PrimaryButton
+                                        type="button"
+                                        variant="warning"
+                                    >
+                                        Edit
+                                    </PrimaryButton>
+                                </Link>
+                            </td>
+                            <td>
+                                <PrimaryButton type="button" variant="danger">
+                                    Delete
+                                </PrimaryButton>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Authenticated>
     );
 }
